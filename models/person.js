64a48d2b424d43486ff7f4c423f989aeb9a9ff00
fahmiben <86672346+fahmiben@users.noreply.github.com>
2021-09-30 23:05:29 +0100

    Add files via upload

---
+++ models/person.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const personSchema = new schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  phone: {type: Number, required: true},
+});
+const Person = mongoose.model("person", personSchema);
+module.exports = Person;
